98a4c02979317b956b1c8922e8295dc1b1682baa
Andi Fajar Nugraha <andifajarlah@gmail.com>
2020-07-30 16:02:17 +0800

    'dashboard'

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,29 +2,121 @@
 
 namespace App\Http\Controllers;
 
+use App\Das;
+use App\Spam;
 use App\Jalan;
+use App\Sumur;
+use App\Embung;
+use App\MataAir;
+use App\Drainase;
 use App\Jembatan;
+use App\Bendungan;
 use App\Pembangunan;
 use App\Pemeliharaan;
+use App\DaerahIrigasi;
 use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $pembangunan = Pembangunan::select('infrastruktur_jenis', DB::raw('count(pembangunan_id) as infrastruktur_jumlah'), DB::raw('sum(pembangunan_nilai) as pembangunan_nilai'))->where('pembangunan_tahun', date('Y'))->groupBy('infrastruktur_jenis')->get();
-        $pemeliharaan = Pemeliharaan::select('infrastruktur_jenis', DB::raw('count(pemeliharaan_id) as infrastruktur_jumlah'), DB::raw('sum(pemeliharaan_nilai) as pemeliharaan_nilai'))->where('pemeliharaan_tahun', date('Y'))->groupBy('infrastruktur_jenis')->get();
+        $jalan = Jalan::get([
+            DB::raw('jalan_nama as nama'),
+            DB::raw('"blue" as warna'),
+            DB::raw('"Jalan" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $jembatan = Jembatan::get([
+            DB::raw('jembatan_nama as nama'),
+            DB::raw('"indigo" as warna'),
+            DB::raw('"Jembatan" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $das = Das::get([
+            DB::raw('das_nama as nama'),
+            DB::raw('"lime" as warna'),
+            DB::raw('"Daerah aliran sungai" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $drainase = Drainase::get([
+            DB::raw('drainase_nama as nama'),
+            DB::raw('"green" as warna'),
+            DB::raw('"Drainase" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $spam = Spam::get([
+            DB::raw('spam_nama_unit as nama'),
+            DB::raw('"orange" as warna'),
+            DB::raw('"SPAM" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $sumur = Sumur::get([
+            DB::raw('sumur_kode as nama'),
+            DB::raw('"yellow" as warna'),
+            DB::raw('"Sumur" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $bendungan = Bendungan::get([
+            DB::raw('bendungan_nama as nama'),
+            DB::raw('"red" as warna'),
+            DB::raw('"Bendungan" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $di = DaerahIrigasi::get([
+            DB::raw('daerah_irigasi_nama as nama'),
+            DB::raw('"pink" as warna'),
+            DB::raw('"Daerah Irigasi" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $embung = Embung::get([
+            DB::raw('embung_nama as nama'),
+            DB::raw('"black" as warna'),
+            DB::raw('"Embung" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
+        $mataair = MataAir::get([
+            'mata_air_nama',
+            DB::raw('"grey" as warna'),
+            DB::raw('"Mata Air" as jenis'),
+            DB::raw('ST_Y(marker) as longitude'),
+            DB::raw('ST_X(marker) as latitude')
+        ]);
 
+        $point = collect($jalan)->merge(collect($jembatan))->merge(collect($das))->merge(collect($drainase))->merge(collect($spam))->merge(collect($sumur))->merge(collect($bendungan))->merge(collect($di))->merge(collect($embung))->merge(collect($mataair));
+
+        $pembangunan = Pembangunan::with('kabupaten_kota')->where('pembangunan_tahun', date('Y'))->get();
+        $pembangunan_group_by_infrastruktur = $pembangunan->groupBy('infrastruktur_jenis')->map(function ($q) {
+            return [
+                'infrastruktur_jenis'  => $q->first()->infrastruktur_jenis,
+                'pembangunan_nilai'  => $q->sum('pembangunan_nilai'),
+            ];
+        });
+        $pemeliharaan = Pemeliharaan::with('kabupaten_kota')->where('pemeliharaan_tahun', date('Y'))->get();
+        $pemeliharaan_group_by_infrastruktur = $pemeliharaan->groupBy('infrastruktur_jenis')->map(function ($q) {
+            return [
+                'infrastruktur_jenis'  => $q->first()->infrastruktur_jenis,
+                'pemeliharaan_nilai'  => $q->sum('pemeliharaan_nilai'),
+            ];
+        });
         $rekap_pembangunan = [];
-        foreach ($pembangunan as $row) {
+        foreach ($pembangunan_group_by_infrastruktur as $row) {
             array_push($rekap_pembangunan,[
-                "name" => $row->infrastruktur_jenis,
-                "y" => $row->pembangunan_nilai,
-                "nilai" => number_format($row->pembangunan_nilai, 2)
+                "name" => $row['infrastruktur_jenis'],
+                "y" => $row['pembangunan_nilai'],
+                "nilai" => number_format($row['pembangunan_nilai'], 2)
             ]);
         }
         $rekap_pemeliharaan = [];
-        foreach ($pemeliharaan as $row) {
+        foreach ($pemeliharaan_group_by_infrastruktur as $row) {
             array_push($rekap_pemeliharaan,[
                 "name" => $row->infrastruktur_jenis,
                 "y" => $row->pemeliharaan_nilai,
@@ -32,14 +124,49 @@ class DashboardController extends Controller
             ]);
         }
 
-        $jalan = Jalan::count();
-        $jembatan = Jembatan::count();
+        $pembangunan_group_by_kabupaten = $pembangunan->groupBy('kabupaten_kota_id')->map(function ($q) {
+            return [
+                'kabupaten_kota'  => $q->first()->kabupaten_kota['kabupaten_kota_nama'],
+                'biaya'  => $q->sum('pembangunan_nilai'),
+            ];
+        });
+        $pemeliharaan_group_by_kabupaten = $pemeliharaan->groupBy('kabupaten_kota_id')->map(function ($q) {
+            return [
+                'kabupaten_kota'  => $q->first()->kabupaten_kota['kabupaten_kota_nama'],
+                'biaya'  => $q->sum('pemeliharaan_nilai'),
+            ];
+        });
+        $rekap_biaya_perkabupaten = [];
+        foreach (collect($pembangunan_group_by_kabupaten)->merge(collect($pemeliharaan_group_by_kabupaten))->groupBy('kabupaten_kota')->map(function ($q) {
+            return [
+                'kabupaten_kota'  => $q->first()['kabupaten_kota'],
+                'jumlah_biaya'  => $q->sum('biaya'),
+            ];
+        }) as $row) {
+            array_push($rekap_biaya_perkabupaten,[
+                'name' => $row['kabupaten_kota'],
+                'y' => $row['jumlah_biaya'],
+                'nilai' => number_format($row['jumlah_biaya'],2)
+            ]);
+        }
+
 		return view('pages.dashboard.index', [
+            'point' => $point,
+            'jalan' => $jalan,
+            'jembatan' => $jembatan,
+            'das' => $das,
+            'drainase' => $drainase,
+            'spam' => $spam,
+            'sumur' => $sumur,
+            'bendungan' => $bendungan,
+            'di' => $di,
+            'embung' => $embung,
+            'mataair' => $mataair,
             'pembangunan' => $pembangunan,
-            'map' => [],
             'pemeliharaan' => $pemeliharaan,
             'rekap_pembangunan' => $rekap_pembangunan,
             'rekap_pemeliharaan' => $rekap_pemeliharaan,
+            'rekap_biaya_perkabupaten' => $rekap_biaya_perkabupaten
         ]);
     }
 }
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -2,10 +2,14 @@
 
 @section('title', ' | Dashboard')
 
+@push('css')
+<link href="{{ url('/public/assets/plugins/leaflet/dist/leaflet.css') }}" rel="stylesheet">
+@endpush
+
 @section('content')
 	<!-- begin breadcrumb -->
 	<ol class="breadcrumb pull-right">
-		<li class="breadcrumb-item"><a href="javascript:;">Home</a></li>
+		<li class="breadcrumb-item"><a href="{{ url('jalan') }}">Home</a></li>
 		<li class="breadcrumb-item active">Dashboard</li>
 	</ol>
 	<!-- end breadcrumb -->
@@ -13,7 +17,7 @@
 	<h1 class="page-header">Dashboard</h1>
 	<!-- end page-header -->
 	<div class="row">
-		<div class="col-lg-6 col-md-6">
+		<div class="col-lg-4 col-md-6">
 			<div class="widget widget-stats bg-red">
 				<div class="stats-icon"><i class="fad fa-digging"></i></div>
 				<div class="stats-info">
@@ -26,8 +30,8 @@
                 </div>
 			</div>
 		</div>
-		<div class="col-lg-6 col-md-6">
-			<div class="widget widget-stats bg-success">
+		<div class="col-lg-4 col-md-6">
+			<div class="widget widget-stats bg-primary">
 				<div class="stats-icon"><i class="fad fa-tools"></i></div>
 				<div class="stats-info">
 					<h4>Pemeliharaan {{ date ('Y')}}</h4>
@@ -39,52 +43,131 @@
                 </div>
 			</div>
 		</div>
+		<div class="col-lg-4 col-md-6">
+			<div class="widget widget-stats bg-success">
+				<div class="stats-icon"><i class="fad fa-tools"></i></div>
+				<div class="stats-info">
+					<h4>Total Biaya {{ date ('Y')}}</h4>
+                    <p class="f-s-24">{{ number_format($pembangunan->sum('pembangunan_nilai') + $pemeliharaan->sum('pemeliharaan_nilai'), 2) }}</p>
+                </div>
+                <div class="stats-link">
+                    <a href="{{ url('/pemeliharaan')}}">View Detail <i class="fa fa-arrow-alt-circle-right"></i></a>
+                </div>
+			</div>
+		</div>
 	</div>
     <div class="row">
-        <div class="col-lg-7 ui-sortable">
+        <div class="col-xl-7 col-lg-12 ui-sortable">
             <div class="panel panel-inverse" data-sortable-id="index-1">
                 <div class="panel-heading">
                     <h4 class="panel-title">
-                        Visitors Origin
+                        Peta Sebaran Infrastruktur
                     </h4>
                 </div>
-                @include('includes.component.leaflet', [ 'y' => 300, 'preview' => 1 ])
-                <div class="list-group">
-                    <a href="javascript:;" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
-                        1. United State
-                        <span class="badge f-w-500 bg-gradient-teal f-s-10">20.95%</span>
-                    </a>
-                    <a href="javascript:;" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
-                        2. India
-                        <span class="badge f-w-500 bg-gradient-blue f-s-10">16.12%</span>
-                    </a>
-                    <a href="javascript:;" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
-                        3. Mongolia
-                        <span class="badge f-w-500 bg-gradient-grey f-s-10">14.99%</span>
-                    </a>
+                <div id="map" class="height-300" style="border: 1px solid rgb(204, 204, 204);" tabindex="0"></div>
+                <div class="row bg-black m-r-0 m-l-0">
+                    <div class="col-xl-4">
+                        <h6 class="text-white m-t-2">BINA MARGA</h6>
+                        <div class="list-group">
+                            <a href="{{ url('jalan') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Jalan
+                            <span class="badge f-w-500 bg-gradient-{{ $jalan->first()->warna }} f-s-10">{{ $jalan->count() }}</span>
+                            </a>
+                            <a href="{{ url('jembatan') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Jembatan
+                                <span class="badge f-w-500 bg-{{ $jembatan->first()->warna }}-blue f-s-10">{{ $jembatan->count() }}</span>
+                            </a>
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-xl-4">
+                        <h6 class="text-white m-t-2">CIPTA KARYA</h6>
+                        <div class="list-group">
+                            <a href="{{ url('das') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Daerah Aliran Sungai
+                            <span class="badge f-w-500 bg-gradient-{{ $das->first()->warna }} f-s-10">{{ $das->count() }}</span>
+                            </a>
+                            <a href="{{ url('drainase') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Drainase
+                            <span class="badge f-w-500 bg-gradient-{{ $drainase->first()->warna }} f-s-10">{{ $drainase->count() }}</span>
+                            </a>
+                            <a href="{{ url('spam') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                SPAM
+                            <span class="badge f-w-500 bg-gradient-{{ $spam->first()->warna }} f-s-10">{{ $spam->count() }}</span>
+                            </a>
+                            <a href="{{ url('sumur') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Sumur
+                            <span class="badge f-w-500 bg-gradient-{{ $sumur->first()->warna }} f-s-10">{{ $sumur->count() }}</span>
+                            </a>
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-xl-4">
+                        <h6 class="text-white m-t-2">SUMBER DAYA AIR</h6>
+                        <div class="list-group">
+                            <a href="{{ url('bendungan') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Bendungan
+                            <span class="badge f-w-500 bg-gradient-{{ $bendungan->first()->warna }} f-s-10">{{ $bendungan->count() }}</span>
+                            </a>
+                            <a href="{{ url('daerahirigasi') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Daerah Irigasi
+                            <span class="badge f-w-500 bg-gradient-{{ $di->first()->warna }} f-s-10">{{ $di->count() }}</span>
+                            </a>
+                            <a href="{{ url('embung') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Embung
+                            <span class="badge f-w-500 bg-gradient-{{ $embung->first()->warna }} f-s-10">{{ $embung->count() }}</span>
+                            </a>
+                            <a href="{{ url('mataair') }}" class="list-group-item list-group-item-inverse d-flex justify-content-between align-items-center text-ellipsis">
+                                Mata Air
+                            <span class="badge f-w-500 bg-gradient-{{ $mataair->first()->warna }} f-s-10">{{ $mataair->count() }}</span>
+                            </a>
+                        </div>
+                        <br>
+                    </div>
                 </div>
             </div>
+            <div id="rekap_biaya_perkabupaten" class="height-250"></div>
         </div>
-        <div class="col-lg-5 ui-sortable">
-            <div class="panel bg-red-transparent-3">
-                <div class="panel-body">
-                    <div id="rekap_pembangunan" class="width-full"></div>
-                </div>
-            </div>
-            <div class="panel bg-green-transparent-3">
-                <div class="panel-body">
-                    <div id="daerahirigasi" class="width-full"></div>
-                </div>
-            </div>
+        <div class="col-xl-5 col-lg-12 ui-sortable">
+            <div id="rekap_pembangunan" class="width-full"></div>
+            <br>
+            <div id="rekap_pemeliharaan" class="width-full"></div>
         </div>
     </div>
 @endsection
 
 @push('scripts')
+<script src="{{ url('/public/assets/plugins/leaflet/dist/leaflet.js') }}"></script>
 	<script src="{{ url('/public/assets/plugins/highcharts/highcharts.js') }}"></script>
 	<script src="{{ url('/public/assets/plugins/highcharts/modules/exporting.js') }}"></script>
     <script src="{{ url('/public/assets/plugins/highcharts/modules/export-data.js') }}"></script>
     <script>
+        var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+
+        osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        osm = L.tileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib }),
+        map = new L.Map('map', { center: new L.LatLng(-8.5783,117.5098), zoom: 8 }),
+        drawnItems = L.featureGroup().addTo(map);
+        L.control.layers({
+            'osm': osm.addTo(map),
+            "google": L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
+                attribution: 'google'
+            })
+        }).addTo(map);
+
+        var point = {!! json_encode($point) !!};
+        point.forEach(element => {
+            if(element.longitude && element.latitude){
+                var marker = L.marker([element.longitude, element.latitude], {
+                    icon: L.divIcon({
+                        className: '',
+                        html : '<div style="font-size: 2em;"><i class="text-' + element.warna + '-darker fa fa-map-marker-alt"></i></div>'
+                    })
+                }).addTo(map);
+                marker.bindPopup('<b>Infrastruktur : </b>' + element.jenis + '<br><b>Nama : </b>' + element.nama);
+            }
+        });
+
         Highcharts.chart('rekap_pembangunan', {
 			chart: {
 				plotBackgroundColor: null,
@@ -93,7 +176,7 @@
 				type: 'pie'
 			},
 			title: {
-				text: 'Porsi Biaya Pembangunan per Infrastruktur'
+				text: 'Porsi Biaya Pembangunan per Infrastruktur tahun {{ date('Y') }}'
 			},
 			tooltip: {
 				pointFormat: '<b>Total Biaya : Rp.{point.nilai}<br>{point.percentage:.1f}%</b>'
@@ -114,7 +197,7 @@
 			}]
 		});
 
-        Highcharts.chart('daerahirigasi', {
+        Highcharts.chart('rekap_pemeliharaan', {
 			chart: {
 				plotBackgroundColor: null,
 				plotBorderWidth: null,
@@ -122,7 +205,7 @@
 				type: 'pie'
 			},
 			title: {
-				text: 'Porsi Biaya Pemeliharaan per Infrastruktur'
+				text: 'Porsi Biaya Pemeliharaan per Infrastruktur tahun {{ date('Y') }}r'
 			},
 			tooltip: {
 				pointFormat: '<b>Total Biaya : Rp.{point.nilai}<br>{point.percentage:.1f}%</b>'
@@ -142,5 +225,55 @@
                     @endphp
 			}]
 		});
+
+        Highcharts.chart('rekap_biaya_perkabupaten', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Porsi Pembiayaan Per Kabupaten'
+            },
+            accessibility: {
+                announceNewData: {
+                    enabled: true
+                }
+            },
+            xAxis: {
+                type: 'category'
+            },
+            yAxis: {
+                title: {
+                    text: 'Total Biaya'
+                }
+
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                series: {
+                    borderWidth: 0,
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.nilai}'
+                    }
+                }
+            },
+
+            tooltip: {
+                headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.nilai}</b><br/>'
+            },
+
+            series: [
+                {
+                    name: "Kabupaten/Kota",
+                    colorByPoint: true,
+                    data: @php
+                        echo json_encode($rekap_biaya_perkabupaten, JSON_NUMERIC_CHECK)
+                    @endphp
+                }
+            ]
+        });
     </script>
 @endpush
